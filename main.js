f34a33f7ba0c43c1f86324d464c59e5bbd5492a4
Aler4 <alerch5666@gmail.com>
2022-11-09 00:31:09 +0300

    case -generic

--- main.js
+++ main.js
@@ -1,71 +1,7 @@
-let d = [
-    {
-        id: 1,
-        name: 'Tolya',
-        description: 'norm pacan',
-        obj: null
-    },
-    {
-        id: 1,
-        name: 'Tolya',
-        description: 'norm pacan',
-        obj: { name: 'Tolya' }
-    },
-    {
-        id: 1,
-        name: 'Stas',
-        description: 'Lorem',
-        obj: { name: 'Vanya', id: { 1: 7 } }
-    },
-    {
-        id: 4,
-        name: 'Sasha',
-        description: 'desc',
-        obj: { name: 'Sasha' }
-    },
-    {
-        id: 3,
-        name: 't',
-        description: 'g',
-        obj: { name: 'a', id: 1 }
-    },
-    {
-        id: 78,
-        name: 'Tanya',
-        description: 'Hmm',
-        obj: { name: 'Vanya', id: 7 }
-    },
-    {
-        id: 78,
-        name: 'Tanya',
-        description: 'Hmm',
-        obj: { name: 'Vanya', id: 1 }
-    },
-    {
-        id: 21,
-        name: 'Vova',
-        description: 'player',
-        obj: { name: 'Nastya', id: 13 }
-    }
-];
-const makeCompare = (obj, item) => {
-    let keys = Object.keys(obj);
-    let amount = 0;
-    for (let key of keys) {
-        if ((obj[key] && typeof obj[key] === 'object') && item[key] !== null && typeof item[key] === 'object') {
-            makeCompare(obj[key], item[key]) && amount++;
-        }
-        obj[key] === item[key] && amount++;
-    }
-    return amount > 0;
+// ** Написати функцію, котра приймає массив обьектів, та повертає массив унікальніх обьектів
+const changeType = (name, cast) => {
+    return cast(name);
 };
-const getUniqObjects = (arr) => {
-    const res = [];
-    let copyArr = arr.slice();
-    for (let item of arr) {
-        let find = copyArr.filter(el => makeCompare(item, el));
-        find.length === 1 && res.push(item);
-    }
-    return res;
-};
-let listOfUniq = getUniqObjects(d);
+let foo = changeType('foo', String);
+let bar = changeType('1', Boolean);
+console.log(foo, bar);
--- main.ts
+++ main.ts
@@ -1,84 +1,98 @@
 
-// Написати функцію, котра приймає массив обьектів, та повертає массив унікальніх обьектів
+// ** Написати функцію, котра приймає массив обьектів, та повертає массив унікальніх обьектів
 
 
-let d: Array<object> = [
-    {
-        id: 1,
-        name: 'Tolya',
-        description: 'norm pacan',
-        obj: null
-    },
-    {
-        id: 1,
-        name: 'Tolya',
-        description: 'norm pacan',
-        obj: { name: 'Tolya' }
-    },
-    {
-        id: 1,
-        name: 'Stas',
-        description: 'Lorem',
-        obj: { name: 'Vanya', id: {1: 7}}
-    },
-    {
-        id: 4,
-        name: 'Sasha',
-        description: 'desc',
-        obj: { name: 'Sasha' }
-    },
-    {
-        id: 3,
-        name: 't',
-        description: 'g',
-        obj: { name: 'a', id: 1}
-    },
-    {
-        id: 78,
-        name: 'Tanya',
-        description: 'Hmm',
-        obj: { name: 'Vanya', id: 7}
-    },
-    {
-        id: 78,
-        name: 'Tanya',
-        description: 'Hmm',
-        obj: { name: 'Vanya', id: 1}
-    },
-    {
-        id: 21,
-        name: 'Vova',
-        description: 'player',
-        obj: { name: 'Nastya', id: 13}
-    }
-];
+// let d: Array<object> = [
+//     {
+//         id: 1,
+//         name: 'Tolya',
+//         description: 'norm pacan',
+//         obj: null
+//     },
+//     {
+//         id: 1,
+//         name: 'Tolya',
+//         description: 'norm pacan',
+//         obj: { name: 'Tolya' }
+//     },
+//     {
+//         id: 1,
+//         name: 'Stas',
+//         description: 'Lorem',
+//         obj: { name: 'Vanya', id: {1: 7}}
+//     },
+//     {
+//         id: 4,
+//         name: 'Sasha',
+//         description: 'desc',
+//         obj: { name: 'Sasha' }
+//     },
+//     {
+//         id: 3,
+//         name: 't',
+//         description: 'g',
+//         obj: { name: 'a', id: 1}
+//     },
+//     {
+//         id: 78,
+//         name: 'Tanya',
+//         description: 'Hmm',
+//         obj: { name: 'Vanya', id: 7}
+//     },
+//     {
+//         id: 78,
+//         name: 'Tanya',
+//         description: 'Hmm',
+//         obj: { name: 'Vanya', id: 1}
+//     },
+//     {
+//         id: 21,
+//         name: 'Vova',
+//         description: 'player',
+//         obj: { name: 'Nastya', id: 13}
+//     }
+// ];
+//
+// const makeCompare = (obj: object, item: object): boolean => {
+//     let keys: Array<string> = Object.keys(obj);
+//     let amount: number = 0;
+//
+//     for (let key of keys) {
+//         if ((obj[key] && typeof obj[key] === 'object') && item[key] !== null && typeof item[key] === 'object') {
+//
+//             makeCompare(obj[key], item[key]) && amount++
+//         }
+//         obj[key] === item[key] && amount++
+//     }
+//
+//     return amount > 0;
+// }
+//
+// const getUniqObjects = (arr): Array<object> | [] => {
+//     const res = [];
+//     let copyArr: Array<object> = arr.slice();
+//
+//     for(let  item of arr) {
+//         let find: Array<object> = copyArr.filter(el => makeCompare(item,el))
+//         find.length === 1 && res.push(item);
+//     }
+//
+//     return res;
+// }
+//
+// let listOfUniq: Array<object> | [] = getUniqObjects(d);
+//
 
-const makeCompare = (obj: object, item: object): boolean => {
-    let keys: Array<string> = Object.keys(obj);
-    let amount: number = 0;
 
-    for (let key of keys) {
-        if ((obj[key] && typeof obj[key] === 'object') && item[key] !== null && typeof item[key] === 'object') {
+// ** Написати дженерик котрий має приймати агрумент та повертати його у зміненному типі
 
-            makeCompare(obj[key], item[key]) && amount++
-        }
-        obj[key] === item[key] && amount++
-    }
 
-    return amount > 0;
+type TPrimitive = string | number | boolean;
+
+interface IConvert<TPrimitive> {
+    (value: String | Boolean | Number): TPrimitive;
 }
 
-const getUniqObjects = (arr): Array<object> | [] => {
-    const res = [];
-    let copyArr: Array<object> = arr.slice();
-
-    for(let  item of arr) {
-        let find: Array<object> = copyArr.filter(el => makeCompare(item,el))
-        find.length === 1 && res.push(item);
-    }
-
-    return res;
+const changeType = <TPrimitive>(name: string | number | boolean, cast: IConvert<TPrimitive>): TPrimitive  => {
+    return cast(name);
 }
-
-let listOfUniq: Array<object> | [] = getUniqObjects(d);
-

1e5af5a361074725d11cc08ac2f1e3f28269020c
Aler4 <alerch5666@gmail.com>
2022-11-09 22:01:28 +0300

    corrected ex #1

--- main.js
+++ main.js
@@ -1,7 +1,84 @@
 // ** Написати функцію, котра приймає массив обьектів, та повертає массив унікальніх обьектів
+let d = [
+    {
+        id: 1,
+        name: 'Tolya',
+        description: 'norm pacan',
+        obj: null
+    },
+    {
+        id: 1,
+        name: 'Tolya',
+        description: 'norm pacan',
+        obj: { name: 'Tolya' }
+    },
+    {
+        id: 1,
+        name: 'Stas',
+        description: 'Lorem',
+        obj: { name: 'Vanya', id: { 1: 7 } }
+    },
+    {
+        id: 4,
+        name: 'Sasha',
+        description: 'desc',
+        obj: { name: 'Sasha' }
+    },
+    {
+        id: 3,
+        name: 't',
+        description: 'g',
+        obj: { name: 'a', id: 1 }
+    },
+    {
+        id: 78,
+        name: 'Tanya',
+        description: 'Hmm',
+        obj: { name: 'Vanya', id: 7 }
+    },
+    {
+        id: 78,
+        name: 'Tanya',
+        description: 'Hmm',
+        obj: { name: 'Vanya', id: 1 }
+    },
+    {
+        id: 21,
+        name: 'Vova',
+        description: 'player',
+        obj: { name: 'Nastya', id: 13 }
+    }
+];
+const makeCompare = (obj, item) => {
+    let keys = Object.keys(obj);
+    let isDif = false;
+    for (let key of keys) {
+        if ((obj[key] && typeof obj[key] === 'object') && item[key] !== null && typeof item[key] === 'object') {
+            isDif = makeCompare(obj[key], item[key]);
+        }
+        else {
+            isDif = obj[key] !== item[key];
+        }
+        if (isDif) {
+            break;
+        }
+    }
+    return isDif;
+};
+const getUniqObjects = (arr) => {
+    const res = [];
+    let arrCopy = arr.slice();
+    for (let i = 0; i <= arr.length - 1; i++) {
+        for (let j = i; j <= arrCopy.length - 1; j++) {
+            if (makeCompare(arr[i], arr[j]) === true) {
+                res.push(...arrCopy.splice(j, 1));
+            }
+        }
+    }
+    return res;
+};
+
+
 const changeType = (name, cast) => {
     return cast(name);
 };
-let foo = changeType('foo', String);
-let bar = changeType('1', Boolean);
-console.log(foo, bar);
--- main.ts
+++ main.ts
@@ -2,86 +2,96 @@
 // ** Написати функцію, котра приймає массив обьектів, та повертає массив унікальніх обьектів
 
 
-// let d: Array<object> = [
-//     {
-//         id: 1,
-//         name: 'Tolya',
-//         description: 'norm pacan',
-//         obj: null
-//     },
-//     {
-//         id: 1,
-//         name: 'Tolya',
-//         description: 'norm pacan',
-//         obj: { name: 'Tolya' }
-//     },
-//     {
-//         id: 1,
-//         name: 'Stas',
-//         description: 'Lorem',
-//         obj: { name: 'Vanya', id: {1: 7}}
-//     },
-//     {
-//         id: 4,
-//         name: 'Sasha',
-//         description: 'desc',
-//         obj: { name: 'Sasha' }
-//     },
-//     {
-//         id: 3,
-//         name: 't',
-//         description: 'g',
-//         obj: { name: 'a', id: 1}
-//     },
-//     {
-//         id: 78,
-//         name: 'Tanya',
-//         description: 'Hmm',
-//         obj: { name: 'Vanya', id: 7}
-//     },
-//     {
-//         id: 78,
-//         name: 'Tanya',
-//         description: 'Hmm',
-//         obj: { name: 'Vanya', id: 1}
-//     },
-//     {
-//         id: 21,
-//         name: 'Vova',
-//         description: 'player',
-//         obj: { name: 'Nastya', id: 13}
-//     }
-// ];
-//
-// const makeCompare = (obj: object, item: object): boolean => {
-//     let keys: Array<string> = Object.keys(obj);
-//     let amount: number = 0;
-//
-//     for (let key of keys) {
-//         if ((obj[key] && typeof obj[key] === 'object') && item[key] !== null && typeof item[key] === 'object') {
-//
-//             makeCompare(obj[key], item[key]) && amount++
-//         }
-//         obj[key] === item[key] && amount++
-//     }
-//
-//     return amount > 0;
-// }
-//
-// const getUniqObjects = (arr): Array<object> | [] => {
-//     const res = [];
-//     let copyArr: Array<object> = arr.slice();
-//
-//     for(let  item of arr) {
-//         let find: Array<object> = copyArr.filter(el => makeCompare(item,el))
-//         find.length === 1 && res.push(item);
-//     }
-//
-//     return res;
-// }
-//
-// let listOfUniq: Array<object> | [] = getUniqObjects(d);
-//
+let d: Array<object> = [
+    {
+        id: 1,
+        name: 'Tolya',
+        description: 'norm pacan',
+        obj: null
+    },
+    {
+        id: 1,
+        name: 'Tolya',
+        description: 'norm pacan',
+        obj: { name: 'Tolya' }
+    },
+    {
+        id: 1,
+        name: 'Stas',
+        description: 'Lorem',
+        obj: { name: 'Vanya', id: {1: 7}}
+    },
+    {
+        id: 4,
+        name: 'Sasha',
+        description: 'desc',
+        obj: { name: 'Sasha' }
+    },
+    {
+        id: 3,
+        name: 't',
+        description: 'g',
+        obj: { name: 'a', id: 1}
+    },
+    {
+        id: 78,
+        name: 'Tanya',
+        description: 'Hmm',
+        obj: { name: 'Vanya', id: 7}
+    },
+    {
+        id: 78,
+        name: 'Tanya',
+        description: 'Hmm',
+        obj: { name: 'Vanya', id: 1}
+    },
+    {
+        id: 21,
+        name: 'Vova',
+        description: 'player',
+        obj: { name: 'Nastya', id: 13}
+    }
+];
+
+const makeCompare = (obj, item) => {
+    let keys = Object.keys(obj);
+    let isDif = false;
+
+
+    for(let key of keys) {
+        if ((obj[key] && typeof obj[key] === 'object') && item[key] !== null && typeof item[key] === 'object') {
+            isDif =  makeCompare(obj[key], item[key]);
+        }
+        else {
+            isDif =  obj[key] !== item[key];
+        }
+
+        if (isDif) {
+            break;
+        }
+    }
+    return isDif;
+};
+const getUniqObjects = (arr) => {
+
+    const res = [];
+    let arrCopy = arr.slice();
+
+    for (let i = 0; i <= arr.length -1; i++) {
+
+        for (let j = i; j <= arrCopy.length - 1; j++) {
+            if (makeCompare(arr[i],arr[j]) === true) {
+                res.push(...arrCopy.splice(j,1))
+            }
+
+        }
+
+    }
+    return res;
+};
+
+let listOfUniq: Array<object> | [] = getUniqObjects(d);
+
 
 
 // ** Написати дженерик котрий має приймати агрумент та повертати його у зміненному типі
